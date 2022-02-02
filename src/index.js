544173830cadaa7fb8a1565ff83fe675b03f3bd2
Ben Prat <bvprat@gmail.com>
2022-02-01 16:33:19 -0800

    Fetch and display repo ID's

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-let repos;
+var repoIdsDiv = document.querySelector('.repos-id-div');
 
-repos = fetch('https://api.github.com/orgs/BoomTownROI/repos')
+let reposData;
+reposData = fetch('https://api.github.com/orgs/BoomTownROI/repos')
   .then((data) => data.json())
   .then((data) => data)
-  .catch((error) => console.log('Sorry, there is an error loading the repos'));
+  .catch((error) => console.log(error.message));
 
-console.log(repos);
+console.log(reposData);
+
+Promise.all([reposData])
+  .then((data) => {
+    repos = data;
+  })
+  .then(() => {
+    displayReposIds(repos);
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
+const displayReposIds = (repos) => {
+  repoIdsDiv.innerHTML = repos[0].map((repo) => {
+    console.log(repo.id);
+    return `
+          <section>
+            <p>Repo ID: ${repo.id}<p>
+          </section>`;
+  });
+};
