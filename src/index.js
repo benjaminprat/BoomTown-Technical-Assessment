6989469a0de1a54fb8b92f8013919741d96906ba
Ben Prat <bvprat@gmail.com>
2022-02-02 12:58:38 -0800

    Add notes for reviewers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
+//Select elements from HTML and assign as variables
+
 var loadDataBtn = document.querySelector('.load-data-btn');
 var verifyDatesBtn = document.querySelector('.verify-dates-btn');
 var compareLengthBtn = document.querySelector('.compare-length-btn');
 
+// Notififying the DOM of click event which invokes clickHandler
 loadDataBtn.addEventListener('click', (event) => {
   handleClick(event);
 });
@@ -13,6 +16,7 @@ compareLengthBtn.addEventListener('click', (event) => {
   handleClick(event);
 });
 
+//Functions invoked dynamically depending on particular button clicked
 const handleClick = (event) => {
   event.preventDefault();
 
@@ -25,6 +29,7 @@ const handleClick = (event) => {
   }
 };
 
+//Helper function to fetch and display data and error messages
 const loadAllData = () => {
   displayReposIds();
   displayEventsIds();
@@ -34,24 +39,7 @@ const loadAllData = () => {
   displayPublicMembersErrorMessage();
 };
 
-const verifyDates = async () => {
-  const verifyResultDiv = document.querySelector('.verify-result-div');
-  let topLevelData = await fetchData('https://api.github.com/orgs/BoomTownROI');
-
-  let originalCreationDate = await topLevelData.created_at;
-  let newCreationDate = new Date(originalCreationDate);
-
-  let originalUpdateDate = await topLevelData.updated_at;
-  let newUpdateDate = new Date(originalUpdateDate);
-
-  if (newCreationDate < newUpdateDate) {
-    verifyResultDiv.innerHTML = `Updated date is more recent than created date.
-  `;
-  } else {
-    verifyResultDiv.innerHTML = `Updated date is less recent than created date.`;
-  }
-};
-
+//Reusable function to parse data or throws error if fetch fails
 const fetchData = async (url) => {
   try {
     const response = await fetch(url);
@@ -62,6 +50,7 @@ const fetchData = async (url) => {
   }
 };
 
+//Displays repository id's after fetch
 const displayReposIds = async () => {
   const repoIdsDiv = document.querySelector('.repos-id-div');
   let reposData = await fetchData(
@@ -70,12 +59,13 @@ const displayReposIds = async () => {
 
   repoIdsDiv.innerHTML = reposData.map((repo) => {
     return `
-    <section class="single-repo">
-    <p>Repo ID: ${repo.id}<p>
-    </section>`;
+      <section class="single-repo">
+        <p>Repo ID: ${repo.id}<p>
+      </section>`;
   });
 };
 
+//Displays event id's after fetch.
 const displayEventsIds = async () => {
   const eventIdsDiv = document.querySelector('.events-id-div');
   let eventsData = await fetchData(
@@ -90,12 +80,12 @@ const displayEventsIds = async () => {
   });
 };
 
+//Displays hooks error message after fetch.
 const displayHooksErrorMessage = async () => {
   const hooksIdsDiv = document.querySelector('.hooks-id-div');
   let hooksData = await fetchData(
     'https://api.github.com/orgs/BoomTownROI/hooks'
   );
-  console.log(hooksData);
 
   if (hooksData.message === 'Not Found') {
     hooksIdsDiv.innerHTML = `<section class="single-hook">
@@ -111,6 +101,7 @@ const displayHooksErrorMessage = async () => {
   }
 };
 
+//Displays issues error after fetch.
 const displayIssuesErrorMessage = async () => {
   const issuesIdsDiv = document.querySelector('.issues-id-div');
 
@@ -132,6 +123,7 @@ const displayIssuesErrorMessage = async () => {
   }
 };
 
+//Displays members error after fetch.
 const displayMembersErrorMessage = async () => {
   const membersIdsDiv = document.querySelector('.members-id-div');
   let membersData = await fetchData(
@@ -152,6 +144,7 @@ const displayMembersErrorMessage = async () => {
   }
 };
 
+//Displays public members error after fetch
 const displayPublicMembersErrorMessage = async () => {
   const publicMembersIdsDiv = document.querySelector('.public-members-id-div');
   let publicMembersData = await fetchData(
@@ -172,8 +165,29 @@ const displayPublicMembersErrorMessage = async () => {
   }
 };
 
+const verifyDates = async () => {
+  const verifyResultDiv = document.querySelector('.verify-result-div');
+  let topLevelData = await fetchData('https://api.github.com/orgs/BoomTownROI');
+
+// Selects given date from top level
+  let originalCreationDate = await topLevelData.created_at;
+  let originalUpdateDate = await topLevelData.updated_at;
+
+//Converts original date for comparison
+  let newCreationDate = new Date(originalCreationDate);
+  let newUpdateDate = new Date(originalUpdateDate);
+
+//Compares dates and displays results
+  if (newCreationDate < newUpdateDate) {
+    verifyResultDiv.innerHTML = `Updated date is more recent than created date.
+  `;
+  } else {
+    verifyResultDiv.innerHTML = `Updated date is less recent than created date.`;
+  }
+};
+
 const allRepositoryData = async () => {
-  //top level object
+  //Top level object
   const publicRepos = await fetchData(
     'https://api.github.com/orgs/BoomTownROI'
   );
@@ -187,6 +201,7 @@ const allRepositoryData = async () => {
     repoCount += await iterateRepos(i);
   }
 
+//Helper function invocation(total repo objects,public repos value(integer))
   compareRepoLength(repoCount, publicRepos.public_repos);
 };
 
@@ -194,7 +209,7 @@ const iterateRepos = async (i) => {
   let data = await fetchData(
     `https://api.github.com/orgs/BoomTownROI/repos?page=${i}`
   );
-
+//Number of objects from fetch of each page.
   return data.length;
 };
 
